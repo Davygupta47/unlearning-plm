52d5c91e087dc73dc170336040ab1f13519a090e
Davygupta47 <dwaipayan.dg07@gmail.com>
2026-03-25 19:43:13 +0530

    setup rq

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from setuptools import setup, find_packages
+
+# setup(
+#     name="unlearning-plm",
+#     version="0.1.0",
+#     packages=[
+#         "unlearning-plm",
+#         "unlearning-plm.methods",
+#         "unlearning-plm.utils",
+#     ],
+     # packages=find_packages(),
+# )
