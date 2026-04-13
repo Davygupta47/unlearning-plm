ce07dd53a1fedd88bba063ac3a78045c809a06d3
Davygupta47 <dwaipayan.dg07@gmail.com>
2026-04-13 19:39:03 +0530

    setup change

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@ from setuptools import setup, find_packages
 setup(
     name="llm_unlearn",
     version="0.1.0",
-    packages=[
-        "llm_unlearn",
-        "llm_unlearn.methods",
-        "llm_unlearn.utils",
+    packages=find_packages(),
+    author="Davygupta47",
+    author_email="dwaipayan.dg07@gmail.com",
+    description="Unlearning in Pre-trained Language Models (PLMs).",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Davygupta47/unlearning-plm",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-     packages=find_packages(),
+    python_requires='>=3.8',
 )
